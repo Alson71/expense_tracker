b57bad0720364b85ce125aebecf0f214bf833233
Elijah Ewers <ewerselijah@gmail.com>
2025-03-27 11:32:51 -0400

    Made expenses list and refractored folders

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget{
@@ -10,13 +12,27 @@ class Expenses extends StatefulWidget{
 }
 
 class _ExpensesState extends State<Expenses>{
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Cheeseburger',
+      amount: 11.99,
+      date: DateTime.now(),
+      category: Category.food
+    ),
+    Expense(
+      title: 'Movie Ticket',
+      amount: 18.99,
+      date: DateTime.now(),
+      category: Category.leisure
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Text("Chart"),
-          Text('Expenses List...'),
+          ExpensesList(expenses: _registeredExpenses),
         ],
       ),
     );
