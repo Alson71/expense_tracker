5775f1dbbc400e8debe8e1ae1c7cd05bf2795614
Elijah Ewers <ewerselijah@gmail.com>
2025-04-08 11:41:40 -0400

    Created new expense item and added controller for title

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -30,9 +30,9 @@ class ExpenseItem extends StatelessWidget {
                   children: [
                     Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
-                    Text(expense.date.toString())
+                    Text(expense.formattedDate),
                   ],
-                )
+                ),
               ],
             )
           ],
